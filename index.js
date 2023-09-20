8dd5c5e7d0c1bb818b3750261ac06df72ed09f66
kawatech002 <kawanjiku94@gmail.com>
2023-09-20 15:22:10 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+//let companyName = 'Scuber';
+const companyName = 'Scuber';
+//let mostProfitableNeighborhood ='chelsea';
+const mostProfitableNeighborhood = 'Chelsea';
+//const companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
